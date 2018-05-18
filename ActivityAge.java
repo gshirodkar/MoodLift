bde74e8078169648857bf1f1fd6080aaaf5db682
iWinger <wc432@scarletmail.rutgers.edu>
2018-05-17 22:39:10 -0400

    Add files via upload

---
+++ ActivityAge.java
@@ -0,0 +1,13 @@
+package wing.moodlift;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityAge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_age);
+    }
+}
